57b42de9f4674111c687ee22aefd2f2a886a7dff
juan2ramos <juan2ramos@gmail.com>
2017-09-18 20:42:44 -0500

    template ok

--- page-blog.php
+++ page-blog.php
@@ -34,6 +34,81 @@ get_header(); ?>
                             </div>
                         </div>
                     </li>
+                    <li class="col-md-4 col-sm-6">
+                        <div class="blog-inter">
+                            <div class="postimg"> <img src="<?php bloginfo('template_url')?>/assets/images/blog/1.jpg" alt="Blog Title">
+                                <div class="date"> 17 SEP</div>
+                            </div>
+                            <div class="post-header">
+                                <h4><a href="/blog-single">Análisis de Arcillas</a></h4>
+                                <div class="postmeta">por : <span>Camilo Quintero</span> </div>
+                            </div>
+                            <div class="postmeta">
+                                <p>Trabajamos como laboratorio de arcillas de las principales ingenierías dedicadas a la fabricación de bienes de equipo para la industria...</p>
+                                <div class="readmore"><a href="/blog-single">Leer más</a></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-md-4 col-sm-6">
+                        <div class="blog-inter">
+                            <div class="postimg"> <img src="<?php bloginfo('template_url')?>/assets/images/blog/1.jpg" alt="Blog Title">
+                                <div class="date"> 17 SEP</div>
+                            </div>
+                            <div class="post-header">
+                                <h4><a href="/blog-single">Análisis de Arcillas</a></h4>
+                                <div class="postmeta">por : <span>Camilo Quintero</span> </div>
+                            </div>
+                            <div class="postmeta">
+                                <p>Trabajamos como laboratorio de arcillas de las principales ingenierías dedicadas a la fabricación de bienes de equipo para la industria...</p>
+                                <div class="readmore"><a href="/blog-single">Leer más</a></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-md-4 col-sm-6">
+                        <div class="blog-inter">
+                            <div class="postimg"> <img src="<?php bloginfo('template_url')?>/assets/images/blog/1.jpg" alt="Blog Title">
+                                <div class="date"> 17 SEP</div>
+                            </div>
+                            <div class="post-header">
+                                <h4><a href="/blog-single">Análisis de Arcillas</a></h4>
+                                <div class="postmeta">por : <span>Camilo Quintero</span> </div>
+                            </div>
+                            <div class="postmeta">
+                                <p>Trabajamos como laboratorio de arcillas de las principales ingenierías dedicadas a la fabricación de bienes de equipo para la industria...</p>
+                                <div class="readmore"><a href="/blog-single">Leer más</a></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-md-4 col-sm-6">
+                        <div class="blog-inter">
+                            <div class="postimg"> <img src="<?php bloginfo('template_url')?>/assets/images/blog/1.jpg" alt="Blog Title">
+                                <div class="date"> 17 SEP</div>
+                            </div>
+                            <div class="post-header">
+                                <h4><a href="/blog-single">Análisis de Arcillas</a></h4>
+                                <div class="postmeta">por : <span>Camilo Quintero</span> </div>
+                            </div>
+                            <div class="postmeta">
+                                <p>Trabajamos como laboratorio de arcillas de las principales ingenierías dedicadas a la fabricación de bienes de equipo para la industria...</p>
+                                <div class="readmore"><a href="/blog-single">Leer más</a></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-md-4 col-sm-6">
+                        <div class="blog-inter">
+                            <div class="postimg"> <img src="<?php bloginfo('template_url')?>/assets/images/blog/1.jpg" alt="Blog Title">
+                                <div class="date"> 17 SEP</div>
+                            </div>
+                            <div class="post-header">
+                                <h4><a href="/blog-single">Análisis de Arcillas</a></h4>
+                                <div class="postmeta">por : <span>Camilo Quintero</span> </div>
+                            </div>
+                            <div class="postmeta">
+                                <p>Trabajamos como laboratorio de arcillas de las principales ingenierías dedicadas a la fabricación de bienes de equipo para la industria...</p>
+                                <div class="readmore"><a href="/blog-single">Leer más</a></div>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
 
                 <!-- Pagination -->
